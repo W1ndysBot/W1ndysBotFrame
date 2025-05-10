1b41b5cd8fc7ca4a2062101da4e9d7c2da059888
W1ndys <w1ndys@outlook.com>
2025-05-10 23:00:03 +0800

    Update start-app-windows.bat

--- start-app-windows.bat
+++ start-app-windows.bat
@@ -1,8 +1,13 @@
 @echo off
 chcp 65001
 
-:: 激活Python虚拟环境
-call venv\Scripts\activate
+:: 检查Python虚拟环境是否存在
+IF EXIST venv\Scripts\activate (
+    echo Activating virtual environment...
+    call venv\Scripts\activate
+) ELSE (
+    echo Virtual environment not found. Using system Python.
+)
 
 :: 进入app目录
 cd app
