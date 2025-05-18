45980b4f9a1a3fa8c13505cffea4ed11b0f703cc
W1ndys <w1ndys@outlook.com>
2025-05-18 23:23:45 +0800

    feat 更新元事件处理器，添加时间属性并完善文档注释，提升代码可读性和功能描述。

--- app/scripts/Example/meta_event_handler.py
+++ app/scripts/Example/meta_event_handler.py
@@ -2,11 +2,15 @@ import logger
 
 
 class MetaEventHandler:
-    """元事件处理器"""
+    """
+    元事件处理器/定时任务处理器
+    元事件可利用心跳来实现定时任务
+    """
 
     def __init__(self, websocket, msg):
         self.websocket = websocket
         self.msg = msg
+        self.time = msg.get("time", "")
         self.post_type = msg.get("post_type", "")
 
     async def handle(self):
