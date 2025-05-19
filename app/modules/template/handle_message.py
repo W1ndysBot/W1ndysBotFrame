7f4786284719f9b5e79457b907f50e00178ae1d0
W1ndys <w1ndys@outlook.com>
2025-05-19 15:03:40 +0800

    feat(消息处理模块): 在群消息和私聊消息处理器中新增对发送者昵称、群名片和身份的提取，增强消息处理的完整性和信息丰富性。

--- app/modules/template/handle_message.py
+++ app/modules/template/handle_message.py
@@ -16,12 +16,17 @@ class MessageHandler:
         处理群消息
         """
         try:
+            self.sub_type = self.msg.get("sub_type", "")  # 子类型，只有normal
             self.group_id = str(self.msg.get("group_id", ""))  # 群号
             self.message_id = str(self.msg.get("message_id", ""))  # 消息ID
             self.user_id = str(self.msg.get("user_id", ""))  # 发送者QQ号
             self.message = self.msg.get("message", {})  # 消息段数组
             self.raw_message = self.msg.get("raw_message", "")  # 原始消息
             self.sender = self.msg.get("sender", {})  # 发送者信息
+            self.nickname = self.sender.get("nickname", "")  # 昵称
+            self.card = self.sender.get("card", "")  # 群名片
+            self.role = self.sender.get("role", "")  # 群身份
+
         except Exception as e:
             logger.error(f"[{MODULE_NAME}]处理群消息失败: {e}")
 
@@ -30,7 +35,13 @@ class MessageHandler:
         处理私聊消息
         """
         try:
-            pass
+            self.sub_type = self.msg.get("sub_type", "")  # 子类型,friend/group
+            self.user_id = str(self.msg.get("user_id", ""))  # 发送者QQ号
+            self.message = self.msg.get("message", {})  # 消息段数组
+            self.raw_message = self.msg.get("raw_message", "")  # 原始消息
+            self.sender = self.msg.get("sender", {})  # 发送者信息
+            self.nickname = self.sender.get("nickname", "")  # 昵称
+
         except Exception as e:
             logger.error(f"[{MODULE_NAME}]处理私聊消息失败: {e}")
 
