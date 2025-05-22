cc7ae7f1e2d463cddff162eb5d427b807401c645
W1ndys <w1ndys@outlook.com>
2025-05-22 16:20:55 +0800

    refactor(模块初始化): 移除开关文件的定义，简化模块初始化代码，提高代码整洁性。

--- app/modules/reporter/__init__.py
+++ app/modules/reporter/__init__.py
@@ -10,8 +10,7 @@ MODULE_DESCRIPTION = "主要用于把私聊bot、加bot为好友，邀请bot入�
 DATA_DIR = os.path.join("data", MODULE_NAME)
 os.makedirs(DATA_DIR, exist_ok=True)
 
-# 开关文件
-SWITCH_FILE = os.path.join(DATA_DIR, "switch.json")
+
 
 
 # 模块的一些命令可以在这里定义，方便在其他地方调用，提高代码的复用率
--- app/modules/template/__init__.py
+++ app/modules/template/__init__.py
@@ -11,8 +11,7 @@ MODULE_DESCRIPTION = "模板模块"
 DATA_DIR = os.path.join("data", MODULE_NAME)
 os.makedirs(DATA_DIR, exist_ok=True)
 
-# 开关文件
-SWITCH_FILE = os.path.join(DATA_DIR, "switch.json")
+
 
 
 # 模块的一些命令可以在这里定义，方便在其他地方调用，提高代码的复用率
