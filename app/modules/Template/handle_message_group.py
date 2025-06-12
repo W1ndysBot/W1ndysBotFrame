518657d3e86cc055ee37f549e7fb8f9008278e20
npc <131228600+Apursuit@users.noreply.github.com>
2025-06-12 18:26:45 +0800

    修复部分遗漏更新的 API 接口引用 (#16)
    * Create .env.example
    
    添加示例 .env 文件
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update .env.example
    
    * Update main.py
    
    * Update main.py
    
    * update
    
    * 修复部分遗漏更新的 API 接口引用
    
    * 恢复 app/main.py 部分
    
    * Create .env.example
    
    ---------
    
    Co-authored-by: W1ndys <w1ndys@outlook.com>
    Co-authored-by: 131228600+Apursuit@users.noreply.github.com <npc@404-Not-Found.localdomain>

--- app/modules/Template/handle_message_group.py
+++ app/modules/Template/handle_message_group.py
@@ -2,7 +2,7 @@ from . import MODULE_NAME, SWITCH_NAME
 from core.menu_manager import MENU_COMMAND
 import logger
 from core.switchs import is_group_switch_on, handle_module_group_switch
-from core.auth import is_system_owner
+from core.auth import is_system_admin
 from api.message import send_group_msg
 from api.generate import generate_text_message, generate_reply_message
 from datetime import datetime
@@ -38,7 +38,7 @@ class GroupMessageHandler:
         try:
             if self.raw_message.lower() == SWITCH_NAME.lower():
                 # 鉴权
-                if not is_system_owner(self.user_id):
+                if not is_system_admin(self.user_id):
                     logger.error(f"[{MODULE_NAME}]{self.user_id}无权限切换群聊开关")
                     return
                 await handle_module_group_switch(
